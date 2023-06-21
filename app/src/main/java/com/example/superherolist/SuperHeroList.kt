ed36378f0267499d7b88bff77f0969eca5daee39
Enderson <gabrielgomez2040@gmail.com>
2023-06-21 13:18:03 -0400

    actividad individual25-version2

--- app/src/main/java/com/example/superherolist/SuperHeroList.kt
+++ app/src/main/java/com/example/superherolist/SuperHeroList.kt
@@ -1,24 +1,22 @@
 package com.example.superherolist
-
+//Creamos la clase para poder crear objetos con su constructor
 data class SuperHero (val superhero:String,
                       val publisher:String,
                       val realName:String,
                       var photo:String){
-
-
-
-
-
-
-
 }
 
+//Creamos la primera funcion que devolvera un arrray con los objetos creados de nuestra clase.
 fun listaSuperheros(): List<SuperHero> {
+    //Declaramos la lista del tipo mutable sobre la clase creada anteriormente.
     val superheroes = mutableListOf<SuperHero>()
+    //Creamos una variable boleeana para crear un ciclo y asi poder cerrarlo.
     var cerrar = false
 
+    //Creamos un ciclo que mientras nuestra variable sea falsa siga reiniciandose.
     while (!cerrar) {
-        println("Ingrese el nombre del superhéroe:")
+        //Preguntamos los datos al usuario y guardamos los resultados en variables para luego construir los objetos de la clase.
+        println("Ingrese el nombre del superheroe:")
         val nombre: String = readLine().toString()
 
         println("Ingrese el editor/publisher del superhéroe:")
@@ -30,9 +28,11 @@ fun listaSuperheros(): List<SuperHero> {
         println("Ingrese la foto del superhéroe:")
         val foto = readLine().toString()
 
+        //Creamos una instancia de nuestra clase y la agregamos al arraylist.
         val superhero = SuperHero(nombre, publisher, nombreReal, foto)
         superheroes.add(superhero)
 
+        //Preguntamos si desea continuar ingresando datos de ser negativo cerramos el ciclo.
         println("¿Desea ingresar otro superhéroe? (Si/No)")
         val opcion = readLine()?.lowercase()
         cerrar = opcion.equals("no")
@@ -43,8 +43,10 @@ fun listaSuperheros(): List<SuperHero> {
     return superheroes
 }
 
+//Funcion que imprime nuestros datos en el arraylist.
 fun mostrarSuperheroes(superheroes: List<SuperHero>) {
     println("Lista de Superhéroes:")
+    //Creamos un ciclo para iterar la lista e ir imprimiendo todos los datos de la misma.
     superheroes.forEachIndexed { index, superhero ->
         println("Superhéroe ${index + 1}:")
         println("Nombre: ${superhero.superhero}")
--- app/src/main/java/com/example/superherolist/main.kt
+++ app/src/main/java/com/example/superherolist/main.kt
@@ -3,28 +3,35 @@ package com.example.superherolist
 
 fun main() {
 
-
+    //Llamamos a la funcion suma.
     println(suma(10.5,10.2f))
 
+    //Llamaoms a funcion suma dentro de la funcion redondeo.
     println(redondeo(suma(10.5,10.3f)))
 
+    //Llamamos a la funcion sumadentro de muestra resultado para darle formato.
     println(muestraResultado(suma(10.5,10.3f)))
 
-
-mostrarSuperheroes(listaSuperheros())
+    //Llamamos a la funcion listasuperheros dentro de mostrar superheroes para que primero
+    // nos pida los datos y luego los imprima por consola.
+    mostrarSuperheroes(listaSuperheros())
 }
-
+//Creamos una funcion para sumar dos tipos de datos diferentes.
 fun suma(numDouble: Double, numFloat: Float): Double {
+    //Convertimos el resultado a double para poder sumarlos.
     var resul: Double = numDouble + numFloat.toDouble()
     return resul
 }
 
+//Creamos una funcion que redondee al numero entero mayor.
 fun redondeo(numDouble: Double): Double {
 
+    //Utilizamos el metodo ceil de la libreria math para hacerlo.
     var resul = kotlin.math.ceil(numDouble)
     return resul
 }
 
+//Creamos una funcion para pasar el resultado a string y darle formato deseado.
 fun muestraResultado(numero: Double):String{
     var resul = String.format("%.2f",numero)
     return resul
